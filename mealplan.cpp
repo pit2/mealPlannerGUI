95ca26e0b44419680613e2057972b6569df296e3
Martin Boehm <marwing64@web.de>
2017-01-17 20:08:10 +0100

    Extract clingy output into multimap<string,multimap<string,string>> data structure
    Add multimap data structure that holds for each weekday (key) another multimap which holds for each time (breakfast etc. as string) a string of the appropriate food item
    Parse the clingo output into this new data structure and display it via qDebug

--- mealplan.cpp
+++ mealplan.cpp
@@ -1,21 +1,72 @@
 #include "mealplan.h"
 #include <QDebug>
 #include <QString>
+#include <map>
 
 MealPlan::MealPlan(string str, int numberOfDays)
 {
-    int current = 0;
-    int lower, higher;
-    string day, food;
-    lower = str.find("breakfast", current); // find a "breakfast" line
-    lower = str.find("\"", lower + 1); // proceed to first " (encapsulating the weekday)
-    higher = str.find("\"", lower + 1); // find second "
-    day = str.substr(lower + 1, higher - lower - 1); // extract weekday
-    lower = str.find(",", lower + 2); // proceed to , as the food item is listed after the ,
-    higher = str.find(")", lower); // find closing )
-    food = str.substr(lower + 1, higher - lower - 1);
-    qDebug() << "identified day as: " << QString::fromStdString(day);
-    qDebug() << "found food: " << QString::fromStdString(food);
+    qDebug() << "Called constructor for mealPlan";
+    multimap<string,string>::iterator it;
+    map<string,multimap<string,string>>::iterator it2;
 
+    weeklyPlan = new multimap<string,multimap<string,string>>();
+    int index = 0;
+    index = findMealsFor(str, "breakfast", index, weeklyPlan);
+    index = findMealsFor(str, "lunch", index, weeklyPlan);
+    findMealsFor(str, "dinner", index, weeklyPlan);
 
+    // DEBUG Output
+    for (it2=weeklyPlan->begin(); it2!=weeklyPlan->end(); ++it2) {
+        qDebug() << "On " << QString::fromStdString((*it2).first);
+        for (it = (*it2).second.begin(); it != (*it2).second.end(); ++it) {
+            qDebug() << "eat for " << QString::fromStdString((*it).first) << " " << QString::fromStdString((*it).second);
+        }
+    }
+}
+
+int MealPlan::findMealsFor(string str, string time, int startIndex, multimap<string,multimap<string,string>> *weeklyPlan) {
+    int lower = startIndex;
+    int higher = lower;
+    string day, prevDay, food;
+    multimap<string,string> partialPlan;
+    multimap<string,string>::iterator it = partialPlan.begin();
+    while(true) {
+
+        lower = str.find(time, lower); // find e.g. a "breakfast" line
+        if (lower == string::npos) { // no more meals for current time slot (across all days)
+            weeklyPlan->insert(pair<string,multimap<string,string>>(prevDay, partialPlan));
+            break;
+        }
+        lower = str.find("\"", lower + 1); // proceed to first " (encapsulating the weekday)
+        higher = str.find("\"", lower + 1); // find second "
+        day = str.substr(lower + 1, higher - lower - 1); // extract weekday
+        if (!prevDay.empty() && prevDay != day) { // add partial plan (e.g. breakfast on a given day) to weeklyPlan
+            qDebug() << "Inserting into weeklyPlan on day " << QString::fromStdString(prevDay);
+            weeklyPlan->insert(pair<string,multimap<string,string>>(prevDay, partialPlan));
+            partialPlan.clear();
+            it = partialPlan.begin();
+        }
+       // qDebug() << "day is " << QString::fromStdString(day);
+       // qDebug() << "prevDay is " << QString::fromStdString(prevDay);
+        prevDay = day;
+        lower = str.find(",", lower + 2); // proceed to , as the food item is listed after the ,
+        higher = str.find(")", lower); // find closing )
+        food = str.substr(lower + 1, higher - lower - 1);
+        it = partialPlan.insert(it, pair<string,string>(time, food));
+
+    }
+
+    qDebug() << "iterating from within findMealsFor";
+
+  /*  multimap<string,string>::iterator it3;
+    map<string,multimap<string,string>>::iterator it2;
+    for (it2=weeklyPlan->begin(); it2 != weeklyPlan->end(); ++it2) {
+        qDebug() << "On " << QString::fromStdString((*it2).first);
+        for (it3 = (*it2).second.begin(); it3 != (*it2).second.end(); ++it3) {
+            qDebug() << "eat for " << QString::fromStdString((*it3).first) << " " << QString::fromStdString((*it3).second);
+        }
+
+    } */
+
+    return higher;
 }
--- mealplan.h
+++ mealplan.h
@@ -2,6 +2,7 @@
 #define MEALPLAN_H
 
 #include <string>
+#include <map>
 
 using namespace std;
 
@@ -13,11 +14,10 @@ public:
 private:
     int startDay = 0;
     int numOfDays = 1;
-    //static string days[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-    //string breakfast[];
-    //string lunch[];
-    //string dinner[];
+    const string days[7] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    multimap<string, multimap<string,string>> *weeklyPlan; // key is a weekday, value the meal plan (whose key is the time (e.g. breakfast) and the food items
 
+    static int findMealsFor(string str, string time, int startIndex, multimap<string, multimap<string, string> > *weeklyPlan);
 };
 
 #endif // MEALPLAN_H
